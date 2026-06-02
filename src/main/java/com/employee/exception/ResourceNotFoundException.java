92b7d87e43088837bf1bcfcb6a9412b4958f19eb
Aniket Gauliwar <aniketgauliwar395@gmail.com>
2026-06-03 02:35:58 +0530

    fixed

--- src/main/java/com/employee/exception/ResourceNotFoundException.java
+++ src/main/java/com/employee/exception/ResourceNotFoundException.java
@@ -6,10 +6,3 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 /**
  * Thrown when a requested resource is not found.
  */
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
